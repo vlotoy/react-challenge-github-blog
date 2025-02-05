6b3536d6787ea17a0f7f0a4bff51e26b14424748
Victor Loyola <victor.tostes@gmail.com>
2025-02-05 18:29:51 -0300

    Added default theme and global styles

---
+++ src/styles/globals.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors['blue']};
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors['base-background']};
+    color: ${({ theme }) => theme.colors['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button { 
+    font: 400 1rem 'Nunito', sans-serif;
+    line-height: 1.6;
+  }
+`
